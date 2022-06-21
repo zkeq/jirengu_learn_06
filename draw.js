6797edae7c8ef9933a03bec3f659ed9f15652195
zkeq <admin@icodeq.com>
2022-06-21 19:58:38 +0800

    添加一堆功能

--- draw.js
+++ draw.js
@@ -1,12 +1,54 @@
 function clear_draw() {
-    console.log("clear_draw");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function save_draw() {
+    var dataURL = canvas.toDataURL();
+    var link = document.createElement('a');
+    link.download = 'draw.png';
+    link.href = dataURL;
+    link.click();
+}
+
+function undo_draw() {
+    if (draw_history.length > 0) {
+        var last_draw = draw_history.pop();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.putImageData(last_draw, 0, 0);
+        redo_history.push(last_draw);
+    }
+    else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function redo_draw() {
+    if (redo_history.length > 0) {
+        var last_draw = redo_history.pop();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.putImageData(last_draw, 0, 0);
+        draw_history.push(last_draw);
+    }
+    else {
+        console.log("no redo");
+    }
+}
+
+function change_color(color) {
+    console.log("color changed to " + color);
+    ctx.strokeStyle = color;
+}
+
+draw_history = [];
+redo_history = [];
+
 // 画线
 let canvas = document.getElementById('canvas');
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 let ctx = canvas.getContext('2d');
 let painting = false;
+let isfrist = true;
 var palt = device.mobile() || device.tablet();
 
 function drawLine(x1, y1, x2, y2) {
@@ -49,6 +91,10 @@ canvas.onmousedown = (e) => {
 };
 
 canvas.onmousemove = (e) => {
+    if (isfrist) {
+        draw_history.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+        isfrist = false;
+    }
     if (painting === true) {
         // ctx.fillRect(e.clientX - 5 , e.clientY - 5, 10, 10);
         drawLine(last[0], last[1], e.clientX, e.clientY);
@@ -61,6 +107,7 @@ canvas.onmousemove = (e) => {
 
 canvas.onmouseup = () => {
     painting = false;
+    isfrist = true;
 };
 
 }
